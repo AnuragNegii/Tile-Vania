1f72d95a6148cad2fe9fa74fe5d99f18fa538e0e
AnuragNegii <negianurag99@gmail.com>
2022-12-14 19:59:17 +0530

    moving player and player input System

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float runSpeed = 10f;
+
+    Vector2 moveInput;
+    Rigidbody2D myRigidBody;
+    
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        Run();
+    }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2 (moveInput.x * runSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
+    }
+    
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+        Debug.Log(moveInput);
+    }
+}
