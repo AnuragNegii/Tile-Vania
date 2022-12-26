2b1e1be2fa0ae6cb449f9b994b56446ddeadf8e1
AnuragNegii <negianurag99@gmail.com>
2022-12-26 12:46:08 +0530

    Added Score and save method for pickup and enemies

--- Assets/Scripts/CoinCollector.cs
+++ Assets/Scripts/CoinCollector.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CoinCollector : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+{   
+    [SerializeField] AudioClip coinSound;
+    [SerializeField] int pointsForCoinPickup = 100;
+    bool wasCollected = false;
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Player" && !wasCollected){
+            wasCollected = true;
+           FindObjectOfType<GameSessions>().AddToScore(pointsForCoinPickup);
+            AudioSource.PlayClipAtPoint(coinSound, Camera.main.transform.position);
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/Scripts/GameSessions.cs
+++ Assets/Scripts/GameSessions.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameSessions : MonoBehaviour
 {
     [SerializeField] int playerLives = 2;
-   
+    [SerializeField] int score = 0;
+    [SerializeField] TextMeshProUGUI livesText;
+    [SerializeField] TextMeshProUGUI scoreText;
+
     void Awake() 
     {
         int numGameSessions = FindObjectsOfType<GameSessions>().Length;
@@ -16,6 +20,11 @@ public class GameSessions : MonoBehaviour
         else{
             DontDestroyOnLoad(gameObject);
         }
+    }
+
+    void Start() {
+        livesText.text = playerLives.ToString();
+        scoreText.text = score.ToString();     
     }
 
     public void ProcessPlayerDeath()
@@ -28,15 +37,23 @@ public class GameSessions : MonoBehaviour
         ResetGameSession();
         }
     }
+
+    public void AddToScore(int pointToAdd){
+        score += pointToAdd;
+        scoreText.text = score.ToString();     
+
+    }
     void TakeLife(){
         playerLives = playerLives -1;//playaerlives--;
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex);
+        livesText.text = playerLives.ToString();
     }
 
     void ResetGameSession(){
         SceneManager.LoadScene(0);
         Destroy(gameObject);
+        FindObjectOfType<ScenePersist>().ResetScenePersist();
     }
 
 }
